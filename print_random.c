d15239ff49f4570a5614f8989da5c410c9d4fe13
BenIncollingo <tup81721@temple.edu>
2024-08-30 14:32:56 -0400

    changes made according to new source code

--- print_random.c
+++ print_random.c
@@ -4,29 +4,15 @@
 #include "random.h"
 #define SIZE 7
 
-char* rand_string_alloc(size_t size);
-void rand_string(char* s, size_t size);
+int main(){
+	int a;
 
-int main(void){
-    srand(time(NULL));
-    char* word = rand_string_alloc(SIZE); //function to allocate memory for word
-    rand_string(word, SIZE); //filling allocated space with characters
-    printf("%s\n", word);
-}
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL));
+	printf("Today's random word: ");
+	for(a=0; a < 7; a++)
+		putchar( randchar() );
+	putchar('\n');
 
-//function to fill empty allocated memory using randchar()
-//randchar is from the random.h header file
-void rand_string(char* s, size_t size){
-    for(size_t i = 0; i < size; ++i){
-        *(s+i) = randchar();
-    }
-}
-
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
-}
+	return(0);
+}	
